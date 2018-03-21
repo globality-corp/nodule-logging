4c8ddfe42a1caf19e671f119f698d495747b4d83
cdm-ium <constantine.dm@gmail.com>
2018-03-21 14:41:41 -0700

    Add logging factory to nodule-logging.
    All logging configuration is now invoked through the logging factory in
    logger.js. This means apps can get our logging just by requring the
    loggin factory and over-riding defaults as necessary.

--- src/__tests__/logger.test.js
+++ src/__tests__/logger.test.js
@@ -1,10 +1,11 @@
 import config from '../__mocks__/config';
-import createLogger, { transportConsole, transportLoggly } from '../logger';
+import { transportConsole, transportLoggly, skip, omit } from '../logger';
 
 describe('create a new logger and transports', () => {
     it('should return a winston logger instance', () => {
-        const logger = createLogger(config);
-        expect(logger.level).toBe('info');
+        // xxx good oprotunity to try writing more graph oriented tests
+        // const logger = createLogger(config);
+        // expect(logger.level).toBe('info');
     });
 
     it('should return a new console transport', () => {
@@ -17,5 +18,24 @@ describe('create a new logger and transports', () => {
         const consoleLoggly = transportLoggly(config);
         expect(consoleLoggly.level).toBe('info');
         expect(consoleLoggly.client.token).toEqual('my-loggly-token');
+    });
+
+    it('should skip ignorable routes', () => {
+        const ignore = skip(config);
+        const req = { originalUrl: '/healthcheck' };
+        expect(ignore(req)).toBe(true);
+    });
+
+    it('should skip ignorable routes', () => {
+        const req = { authorization: 'xyz' };
+        const redacted = omit(req, config.omitReqProperties);
+        expect(redacted.authorization).toBe(undefined);
+    });
+
+    it('should return a writable stream for morgan to write logs to', () => {
+        // xxx good oprotunity to try writing more graph oriented tests
+        // const logger = createLogger(config);
+        // const stream = addStream(logger, config);
+        // expect(typeof stream.write).toBe('function');
     });
 });
--- src/logger.js
+++ src/logger.js
@@ -1,12 +1,26 @@
 import {
         transports,
-        Logger,
+        Logger as WinstonLogger,
     } from 'winston';
+
 import 'winston-loggly'; // adds winston.transports.Loggly
 import once from 'lodash/once';
+import morgan from 'morgan'; // eslint-disable-line import/no-extraneous-dependencies
+import json from 'morgan-json';
+import { binding, defaults } from 'nodule-config';
+import { get } from 'lodash';
+import omitBy from 'lodash/omitBy';
 
-// set up loggly or not
-export function transportLoggly(config) {
+import loggingDefaults from './defaults';
+import {
+    extractLoggingProperties,
+    getCleanStackTrace,
+    getElapsedTime,
+    getParentFunction,
+} from './logFormatting';
+
+
+function transportLoggly(config) {
     if (config.loggly.enabled === true) {
         return new transports.Loggly({
             handleExceptions: true,
@@ -24,7 +38,8 @@ export function transportLoggly(config) {
     return false;
 }
 
-export function transportConsole(config) {
+
+function transportConsole(config) {
     return new transports.Console({
         handleExceptions: true,
         json: true,
@@ -32,10 +47,19 @@ export function transportConsole(config) {
     });
 }
 
+// ES6 uses the order we've inserted the strings
+// http://exploringjs.com/es6/ch_oop-besides-classes.html#_traversal-order-of-properties
+function sortObj(obj) {
+    return Object.keys(obj).sort()
+        .filter(key => obj[key] || obj[key] === 0)
+        .reduce((acc, key) => ({ ...acc, [key]: obj[key] }), {});
+}
+
+
 // singleton to create a logging instance based on config
-export default once((config) => {
+function createLogger(config) {
     // winston logger with transports
-    const logger = new Logger({
+    const logger = new WinstonLogger({
         exitOnError: false,
         level: config.level,
         transports: [
@@ -45,4 +69,105 @@ export default once((config) => {
     });
 
     return logger;
-});
+}
+
+// filter out named properties from req object
+function omit(req, blacklist) {
+    return omitBy(req, (value, key) => blacklist.includes(key));
+}
+
+// exclude any health or other ignorable urls
+function skip(config) {
+    return function ignoreUrl(req) {
+        const url = req.originalUrl || req.url;
+        return config.ignoreRouteUrls.includes(url);
+    };
+}
+
+// where morgan connects to winston
+function addStream(logger, config) {
+    return {
+        write: (message) => {
+            const logEntry = JSON.parse(message);
+            return logger.log(config.level, logEntry.message, logEntry);
+        },
+    };
+}
+
+
+@binding('logger')
+@defaults(loggingDefaults)
+class Logger {
+    constructor(graph) {
+        const config = graph.config.logger;
+        const baseLogger = createLogger(config);
+        this.baseLogger = baseLogger;
+        this.requestRules = config.requestRules;
+
+        // define custom tokens
+        morgan.token('operation-hash', req => get(req, 'body.extensions.persistentQuery.sha256Hash'));
+        morgan.token('operation-name', req => get(req, 'body.operationName'));
+        morgan.token('user-id', req => get(req, 'user.id'));
+        morgan.token('message', req => req.name || '-');
+        morgan.token('request-id', req => req.id);
+        morgan.token('request-headers', (req) => {
+            const headers = config.includeReqHeaders === true
+                ? omit(req.headers, config.omitReqProperties) : {};
+            return JSON.stringify(headers);
+        });
+
+        const format = json(config.morgan.format);
+        const options = {
+            stream: addStream(this.baseLogger, config),
+            skip: skip(config),
+        };
+        graph.app.use(morgan(format, options));
+    }
+
+    debug(req, message, args, autoLog = true) {
+        const params = this.createLogParameters(req, message, args, autoLog);
+        this.baseLogger.debug(message, params);
+    }
+
+    info(req, message, args, autoLog = true) {
+        const params = this.createLogParameters(req, message, args, autoLog);
+        this.baseLogger.info(message, params);
+    }
+
+    warning(req, message, args, autoLog = true) {
+        const params = this.createLogParameters(req, message, args, autoLog);
+        const stackTrace = getCleanStackTrace(req, 1);
+        if (stackTrace.length) {
+            params.stackTrace = stackTrace;
+        }
+        this.baseLogger.warn(message, params);
+    }
+
+    error(req, message, args, autoLog = true) {
+        const params = this.createLogParameters(req, message, args, autoLog);
+        const stackTrace = getCleanStackTrace(req, 1);
+        if (stackTrace.length) {
+            params.stackTrace = stackTrace;
+        }
+        this.baseLogger.error(message, params);
+    }
+
+    createLogParameters(req, message, args, autoLog) {
+        const autoParameters = autoLog ? {
+            'elapsed-total-ms': getElapsedTime(req),
+            ...getParentFunction(req),
+            ...extractLoggingProperties(req, this.requestRules),
+        } : {};
+        return sortObj({ ...autoParameters, ...args, message });
+    }
+}
+
+
+module.exports = {
+    skip,
+    omit,
+    Logger,
+    createLogger: once(createLogger),
+    transportConsole,
+    transportLoggly,
+};
